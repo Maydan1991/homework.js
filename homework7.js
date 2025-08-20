9c80720eb032cfe98387c274c2fd0ab94e9f850d
Oleksii Maidanevich <maidanevych12@gmail.com>
2025-08-21 02:15:22 +0300

    Додано homework7.js

--- homework7.js
+++ homework7.js
@@ -1,9 +1,10 @@
 "use strict";
 
-var re = /[b-zB-Z]{6,}/;
-console.log(/Wonderful,re/.test("Wonderful, re")); 
-console.log(/Joyful,re/.test("Joyful, re"));
-console.log(/Happiness,re/.test("Happiness, re")); 
-console.log(/Time,re/.test("Time, re")); 
-console.log(/Task,re/.test("Task, re"));
-console.log(/Apple,re/.test("Apple, re"));    
+var re = /^[b-zB-Z]{6,}$/;
+
+console.log("Wonderful", re.test("Wonderful")); // true
+console.log("Joyful", re.test("Joyful"));       // true
+console.log("Happiness", re.test("Happiness")); // false
+console.log("Time", re.test("Time"));           // false
+console.log("Task", re.test("Task"));           // false
+console.log("Apple", re.test("Apple"));     
